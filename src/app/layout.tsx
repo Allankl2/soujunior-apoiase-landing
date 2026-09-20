8397b387ad61951a599ace3acff40b80351aa5fc
Simara Santos Silva <simaraufes88@gmail.com>
2026-09-20 13:40:18 -0300

    feat: adicionar estilos globais e animações na seção hero e ajustes no cabeçalho

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 
+import "./globals.css";
+
 export const metadata: Metadata = {
   title: "SouJunior Apoia.se",
   description: "Landing page do SouJunior Apoia.se",
