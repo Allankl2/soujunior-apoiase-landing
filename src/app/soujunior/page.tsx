ebe67b289334f5f6d75c357fe74955b9df191cb6
allan <allanf333@gmail.com>
2026-09-22 23:48:20 -0300

    trocado section iniciativasSection

--- src/app/soujunior/page.tsx
+++ src/app/soujunior/page.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@/app/soujunior/_components/header/header";
 import { HeroSection } from "@/app/soujunior/_components/hero-section/hero-section";
 import { About } from "@/app/soujunior/_components/about-section/about";
+import { IniciativasSection } from '@/app/soujunior/_components/problem-section/iniciativas-section';
 import { Transparency } from "@/app/soujunior/_components/transparency-section/transparency";
 import { Closing } from "@/app/soujunior/_components/closing-section/closing";
 import { Participate } from "@/app/soujunior/_components/participate-section/participate";
@@ -14,6 +15,9 @@ export default function Home() {
       <main>
         <HeroSection />
         <About />
+
+        <IniciativasSection />
+
         <Participate />
         <Transparency />
         <Closing />
